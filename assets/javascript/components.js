6594c51f0162dc14f725492cf8b1f041ce07f8ee
Pascal Garber <pascal@jumplink.eu>
2018-02-19 12:23:08 -0500

    [Events] Working Gallery

--- assets/javascript/components.js
+++ assets/javascript/components.js
@@ -734,6 +734,10 @@ rivets.components['firebase-events-beautiful'] = {
     controller.type = data.type;
     controller.calendar = data.calendar;
     controller.excludeCalendar = data.excludeCalendar;
+    controller.showGallery = data.showGallery === true || data.showGallery === 'true' || data.showGallery === 1;
+    controller.galleryTitle = data.galleryTitle;
+    controller.galleryText = data.galleryText;
+    
     // start time of the orders in the future, from the past or all
     controller.startTime = data.startTime || 'future'; // 'future' | 'past' | 'all'
     controller.style = data.style;
@@ -887,7 +891,8 @@ rivets.components['firebase-events-beautiful-gallery'] = {
     controller.debug = debug('rivets:firebase-events-beautiful-gallery');
     controller.debug('initialize', el, data);
     var $el = $(el);
-    
+    var $imagesWrapper = $el.find('.images-row');
+
     controller.ready = false;
     controller.containerClass = data.containerClass || 'container';
     controller.title = data.title;
@@ -895,54 +900,99 @@ rivets.components['firebase-events-beautiful-gallery'] = {
     controller.calendar = data.calendar;
     controller.style = data.style;
     controller.events = data.events;
+    controller.headerTitle = data.headerTitle;
+    controller.headerText = data.headerText;
     
     controller.images = [];
-    controller.photoSwipeImages = [];
     
-    controller.events.forEach(function(event) {
-        controller.debug('event', event);
-        event.images.forEach(function(image) {
-            controller.images.push(image);
-            var photoSwipeImage = {
-                src: image.downloadURL,
-                w: 800,
-                h: 600,
-            };
-            controller.photoSwipeImages.push(photoSwipeImage);
+    var getImagesFromEvents = function(events) {
+        var images = [];
+        events.forEach(function(event) {
+            controller.debug('event', event);
+            /** converts images to the photoswipe format */
+            event.images.forEach(function(image, index) {        
+                /** path to image */
+                image.src = image.downloadURL;
+                /** path to small image placeholder, large image will be loaded on top */
+                image.msrc = image.downloadURL;
+                /** image width */
+                image.w = image.customMetadata.width || 800;
+                /** image height */
+                image.h = image.customMetadata.height || 600;
+                /** image caption */
+                image.title = image.metadata.name;
+                /** custom HTML @see http://photoswipe.com/documentation/custom-html-in-slides.html */
+                // image.html = '';
+                /** save link to element for getThumbBoundsFn */
+                // el:
+                image.index = index;
+    
+                images.push(image);
+            });
         });
-       
-    });
+        return images;
+    }
+    
+    // triggers when user clicks on thumbnail
+    controller.onThumbnailsClick = function(e) {
+        e = e || window.event;
+        e.preventDefault ? e.preventDefault() : e.returnValue = false;
+
+        var $target = $(e.target || e.srcElement);
+        var data = $target.data();
+
+        controller.debug('[onThumbnailsClick] $target', $target, data);
+
+        if(data.index >= 0) {
+            // open PhotoSwipe if valid index found
+            openPhotoSwipe(data.index);
+        }
+        return false;
+    };
+
+    var openPhotoSwipe = function(index) {
+        $.event.trigger('rivets:photoswipe:open', [$imagesWrapper, index, controller.images]);
+    };
     
     // TODO move to rv-img component
-    setTimeout(function(){
-        $el.find('.lazy').Lazy();
+    setTimeout(function() {
+        $imagesWrapper.find('.lazy').Lazy({
+            scrollDirection: 'vertical',
+            effect: "fadeIn",
+            effectTime: 600,
+            threshold: 0,
+            visibleOnly: true,
+            bind: 'event',
+            delay: -1,
+            /**  called before an elements gets handled */
+            beforeLoad: function(element) {
+                // var imageSrc = element.data('src');
+                // controller.debug('[Lazy] image "' + imageSrc + '" is about to be loaded');
+            },
+            
+            /**  called after an element was successfully handled */
+            afterLoad: function(element) {
+                // var imageSrc = element.data('src');
+                // controller.debug('[Lazy] image "' + imageSrc + '" was loaded successfully');
+            },
+            
+            /** called whenever an element could not be handled */
+            onError: function(element) {
+                var imageSrc = element.data('src');
+                controller.debug('[Lazy] image "' + imageSrc + '" could not be loaded');
+            },
+            
+            /**  called once all elements was handled */
+            onFinishedAll: function() {
+                // controller.debug('[Lazy] finished loading all images');
+            },
+        });
          controller.ready = true;
     }, 100);
     
     
-
-    var openPhotoSwipe = function() {
-        var pswpElement = $el.find('.pswp')[0];
     
-
-        // define options (if needed)
-        var options = {
-             // history & focus options are disabled on CodePen        
-            history: false,
-            focus: false,
-    
-            showAnimationDuration: 0,
-            hideAnimationDuration: 0
-            
-        };
-        
-        var gallery = new PhotoSwipe( pswpElement, PhotoSwipeUI_Default, controller.photoSwipeImages, options);
-        gallery.init();
-    };
-    
-    openPhotoSwipe();
-    
-    
+    controller.images = getImagesFromEvents(controller.events);
     controller.debug('images', controller.images);
 
     return controller;
@@ -1089,7 +1139,7 @@ rivets.components['file-upload'] = {
     
     var addFilesToUpload = function(files) {
         var files = Array.from(files);
-        controller.debug('addFiles', files);
+        controller.debug('[addFilesToUpload]', files);
         controller.droppedFiles = files;
         previewFiles(files);
     };
@@ -1112,12 +1162,20 @@ rivets.components['file-upload'] = {
         files.forEach(function(file) {
             var reader = new FileReader();
             reader.onload = function(e) {
-                previewFile = {
-                    dataURL: reader.result,
-                    name: file.name,
-                    size: file.size,
+                var img = new Image;
+                // load image to get width and height
+                img.onload = function() {
+                    previewFile = {
+                        dataURL: reader.result,
+                        name: file.name,
+                        size: file.size,
+                        width: img.width,
+                        height: img.height,
+                    }
+                    controller.debug('[previewFiles] img.onload', previewFile);
+                    controller.previewFiles.push(previewFile);
                 }
-                controller.previewFiles.push(previewFile);
+                img.src = reader.result;
             }
             reader.readAsDataURL(file);
         });
@@ -1156,23 +1214,36 @@ rivets.components['file-upload'] = {
                 return dbImages.add(uploadedFile);
             })
             .then(function(docRef) {
+                var currentPreviewFiles;
+                var currentDroppedFiles;
+                
                 uploadedFile.id = docRef.id;
                 addUploadedFile(uploadedFile);
-                
-                var message = 'Bild ' + uploadedFile.metadata.name + ' erfolgreich eingetragen';
-                controller.debug(message, uploadedFile);
+                        
                 
                 // remove file from upload stack
                 var droppedFileIndex = getIndexByName(controller.droppedFiles, file);
                 if(droppedFileIndex !== -1) {
-                    controller.droppedFiles.splice(droppedFileIndex, 1);
+                    controller.debug('droppedFileIndex', droppedFileIndex);
+                    currentDroppedFile = controller.droppedFiles.splice(droppedFileIndex, 1);
                 }
                 
-                // remove file from preview stack
+                /**
+                 * remove file from preview stack
+                 * and get width and height from preview image to store in database
+                 */
                 var previewFileIndex = getIndexByName(controller.previewFiles, file);
                 if(previewFileIndex !== -1) {
-                     controller.previewFiles.splice(previewFileIndex, 1);
+                     currentPreviewFiles = controller.previewFiles.splice(previewFileIndex, 1);
+                     controller.debug('currentPreviewFiles', currentPreviewFiles);
+                     uploadedFile.customMetadata = {
+                        width: currentPreviewFiles[0].width,
+                        height: currentPreviewFiles[0].height,
+                     }
                 }
+                
+                var message = 'Bild ' + uploadedFile.metadata.name + ' erfolgreich eingetragen';
+                controller.debug(message, uploadedFile);
 
                 return uploadedFile;
             });
@@ -1363,6 +1434,270 @@ rivets.components['preview-file'] = {
     controller.debug('initialize', el, data);
     var $el = $(el);
     controller.file = data.file;
+    
+    return controller;
+  }
+};
+
+/**
+ * Component to open a photoswipe gallery
+ * 
+ * @events
+ *  * rivets:photoswipe:open (event, $imgWrapper, index, images)
+ */
+rivets.components['rv-photoswipe'] = {
+
+  template: function() {
+    return $('template#rv-photoswipe').html();
+  },
+
+  initialize: function(el, data) {
+    var controller = this;
+    controller.debug = debug('rivets:rv-photoswipe');
+    controller.debug('initialize', el, data);
+    var $el = $(el);
+    
+    // photoswipe element
+    var $pswp = $el.find('.pswp');
+    var pswp;
+    var $imagesWrapper;
+    
+    /** define options (if needed) @see http://photoswipe.com/documentation/options.html */
+    var options = {
+         // history & focus options are disabled on CodePen        
+        history: false,
+        focus: false,
+        index: 0,
+
+        /** If set to false: background opacity and image scale will be animated (image opacity is always 1). */
+        // showHideOpacity: true,
+        
+        /** Initial zoom-in transition duration in milliseconds. Set to 0 to disable. */
+        // showAnimationDuration: 0,
+        
+        /** The same as the previous option, just for closing (zoom-out) transition. */
+        // hideAnimationDuration: 0
+        
+        /** Background (.pswp__bg) opacity. Should be a number from 0 to 1, e.g. 0.7. */
+        bgOpacity: 1,
+        
+        /** Spacing ratio between slides. For example, 0.12 will render as a 12% of sliding viewport width (rounded). */
+        spacing: 0.12,
+        
+        /** Allow swipe navigation to next/prev item when current item is zoomed. Option is always false on devices that don't have hardware touch support. */
+        allowPanToNext: true,
+        
+        /** Maximum zoom level when performing spread (zoom) gesture. 2 means that image can be zoomed 2x from original size.  */
+        maxSpreadZoom: 2,
+        
+        // getDoubleTapZoom
+        
+        loop: true,
+        pinchToClose: true,
+        closeOnScroll: true,
+        closeOnVerticalDrag: true,
+        mouseUsed: false,
+        escKey: true,
+        arrowKeys: true,
+        history: true,
+        galleryUID: 1,
+        galleryPIDs: true,
+        errorMsg: '<div class="pswp__error-msg"><a href="%url%" target="_blank">Das Bild</a> konnte nicht geladen werden.</div>',
+        preload: [1,1],
+        //mainClass: '',
+        // getNumItemsFn: function(){},
+        focus: true,
+        isClickableElement: function(el) {
+            return el.tagName === 'A';
+        },
+        
+        /**
+         * Controls whether PhotoSwipe should expand to take up the entire viewport.
+         * If false, the PhotoSwipe element will take the size of the positioned parent of the template.
+         * Take a look at the FAQ for more information.
+         */
+        modal: true,
+        
+        /**
+         * Function should return an object with coordinates from which initial zoom-in animation will start (or zoom-out animation will end).
+         */
+        getThumbBoundsFn: function(index) {
+            controller.debug('[getThumbBoundsFn] index', index, '$imagesWrapper', $imagesWrapper);
+            // find thumbnail element
+            var $thumbnail = $imagesWrapper.find('[data-index="'+ index +'"]');
+            controller.debug('[getThumbBoundsFn] $thumbnail', $thumbnail);
+            // get window scroll Y
+            var pageYScroll = window.pageYOffset || document.documentElement.scrollTop; 
+            // optionally get horizontal scroll
+            // get position of element relative to viewport
+            var rect = $thumbnail[0].getBoundingClientRect();
+            controller.debug('[getThumbBoundsFn] rect', rect);
+            // w = width
+            return {x:rect.left, y:rect.top + pageYScroll, w:rect.width};
+            // Good guide on how to get element coordinates:
+            // http://javascript.info/tutorial/coordinates
+        },
+        
+        /**
+         * Size of top & bottom bars in pixels,
+         * 
+         * "bottom" parameter can be 'auto' (will calculate height of caption)
+         * option applies only when mouse is used,
+         * or width of screen is more than 1200px
+         * (Also refer to `parseVerticalMargin` event)
+         */
+        barsSize: {top:44, bottom:'auto'}, 
+        
+        // Adds class pswp__ui--idle to pswp__ui element when mouse isn't moving for 4000ms
+        timeToIdle: 4000,
+        
+        // Same as above, but this timer applies when mouse leaves the window
+        timeToIdleOutside: 1000,
+        
+        // Delay until loading indicator is displayed
+        loadingIndicatorDelay: 1000,
+        
+        // Function builds caption markup
+        addCaptionHTMLFn: function(item, captionEl, isFake) {
+            // item      - slide object
+            // captionEl - caption DOM element
+            // isFake    - true when content is added to fake caption container
+            //             (used to get size of next or previous caption)
+        
+            if(!item.title) {
+                captionEl.children[0].innerHTML = '';
+                return false;
+            }
+            captionEl.children[0].innerHTML = item.title;
+            return true;
+        },
+        
+        // Buttons/elements
+        closeEl:true,
+        captionEl: true,
+        fullscreenEl: true,
+        zoomEl: true,
+        shareEl: false,
+        counterEl: true,
+        arrowEl: true,
+        preloaderEl: true,
+        
+        // Tap on sliding area should close gallery
+        tapToClose: false,
+        
+        // Tap should toggle visibility of controls
+        tapToToggleControls: true,
+        
+        /**
+         * Mouse click on image should close the gallery,
+         * only when image is smaller than size of the viewport
+         */
+        clickToCloseNonZoomable: true,
+        
+        /**
+         * Element classes click on which should close the PhotoSwipe.
+         * In HTML markup, class should always start with "pswp__", e.g.: "pswp__item", "pswp__caption".
+         * "pswp__ui--over-close" class will be added to root element of UI when mouse is over one of these elements
+         * By default it's used to highlight the close button.
+         */
+        closeElClasses: ['item', 'caption', 'zoom-wrap', 'ui', 'top-bar'], 
+        
+        /**
+         * Separator for "1 of X" counter
+         */
+        indexIndicatorSep: ' / ',
+        
+        /**
+         * Share buttons
+         * Available variables for URL:
+         * {{url}}             - url to current page
+         * {{text}}            - title
+         * {{image_url}}       - encoded image url
+         * {{raw_image_url}}   - raw image url
+         */
+        shareButtons: [
+            {id:'facebook', label:'Share on Facebook', url:'https://www.facebook.com/sharer/sharer.php?u={{url}}'},
+            {id:'twitter', label:'Tweet', url:'https://twitter.com/intent/tweet?text={{text}}&url={{url}}'},
+            {id:'pinterest', label:'Pin it', url:'http://www.pinterest.com/pin/create/button/?url={{url}}&media={{image_url}}&description={{text}}'},
+            {id:'download', label:'Download image', url:'{{raw_image_url}}', download:true}
+        ],
+        
+        
+
+        /**
+         * Next 3 functions return data for share links
+         * 
+         * functions are triggered after click on button that opens share modal,
+         * which means that data should be about current (active) slide
+         */
+        getImageURLForShare: function( shareButtonData ) {
+            // `shareButtonData` - object from shareButtons array
+            // 
+            // `pswp` is the gallery instance object,
+            // you should define it by yourself
+            // 
+            return pswp.currItem.src || '';
+        },
+        
+        getPageURLForShare: function( shareButtonData ) {
+            return window.location.href;
+        },
+        getTextForShare: function( shareButtonData ) {
+            return pswp.currItem.title || '';
+        },
+        
+        /**
+         * Parse output of share links
+         */
+        parseShareButtonOut: function(shareButtonData, shareButtonOut) {
+            // `shareButtonData` - object from shareButtons array
+            // `shareButtonOut` - raw string of share link element
+            return shareButtonOut;
+        }
+        
+    };
+    
+    var open = function(index) {
+        options.index = index;
+        pswp = new PhotoSwipe( $pswp[0], PhotoSwipeUI_Default, controller.images, options);
+        pswp.init();
+    };
+    
+    controller.next = function(event, controller) {
+        pswp.next();
+    }
+    
+    controller.prev = function(event, controller) {
+        pswp.prev();
+    }
+    
+    controller.close = function(event, controller) {
+        pswp.close();
+    }
+    
+    controller.toggleZoom = function(event, controller) {
+        pswp.toggleDesktopZoom();
+    }
+
+    controller.toggleFullscreen = function(event, controller) {
+        var _fullscrenAPI = pswp.ui.getFullscreenAPI();
+
+		if(_fullscrenAPI.isFullscreen()) {
+			_fullscrenAPI.exit();
+		} else {
+			_fullscrenAPI.enter();
+		}
+		
+        pswp.ui.updateFullscreen();
+    }
+    
+    
+    $(document).bind('rivets:photoswipe:open', function (event, $imgWrapper, index, images) {
+        controller.debug('[rivets:photoswipe:open]', event, index, images);
+        $imagesWrapper = $imgWrapper;
+        controller.images = images;
+        open(index);
+    });
     
     return controller;
   }
